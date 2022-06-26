c62f9a9a066c18b6dbfbe0ba73aa8eec40005ce8
alessio <alessiomolinar@gmail.com>
2022-06-26 13:13:17 +0200

    saving for checkout

--- tmt/decorators/recorder.py
+++ tmt/decorators/recorder.py
@@ -6,6 +6,16 @@ from datetime import datetime
 
 
 def recorder(name: str, config_path: Optional[str] = None, save_on_exception=False):
+    """One of the main `tmt` functions. This is a decorator which can be used to keep track of 
+    experiments, saving metrics, results and taking a snapshot of the code at this moment in time
+
+    :param name: name used to save this experiment in the database
+    :type name: str
+    :param config_path: if you want to use a custom configuration file, specify the path. Defaults to None
+    :type config_path: Optional[str], optional
+    :param save_on_exception: save everything (snapshot, metrics etc.) even if an exception happens. Defaults to False
+    :type save_on_exception: bool, optional
+    """
     def inner(func: Callable[..., Optional[Dict[str, float]]]):
         def wrapper(*args, **kwargs) -> Optional[Dict[str, float]]:
             cm = ContextManager(name, config_path)
@@ -23,6 +33,7 @@ def recorder(name: str, config_path: Optional[str] = None, save_on_exception=Fal
             except Exception as e:
                 if not save_on_exception:
                     raise e
+                cm.snap_manager.make_snapshot()
                 cm.entry.date_saved = int(datetime.now().timestamp())
                 db_manager.add_new_entries([cm.entry])
             
